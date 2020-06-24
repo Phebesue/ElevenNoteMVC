05d1cf3fc90c4a8d26c6b73019924a196ba1cedf
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-06-24 12:33:46 -0400

    Update Note Controller add Note Index View

---
+++ MVC_ElevenNote/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ElevenNote.Controllers
+{
+    public class NoteController : Controller
+    {
+        [Authorize]
+        // GET: Note/Index
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
