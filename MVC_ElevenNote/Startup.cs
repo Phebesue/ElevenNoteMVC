b63c046a33690166d6f3f5d7b44f35e18463d92c
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-06-24 10:31:30 -0400

    Add project files.

---
+++ MVC_ElevenNote/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_ElevenNote.Startup))]
+namespace MVC_ElevenNote
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
